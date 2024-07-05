61d7b81da55d98016f60c8411dd6a84d2a2f1869
GiToon10100011 <boon10034@gmail.com>
2024-07-06 02:15:56 +0900

    0706 Update

--- Code/script.js
+++ Code/script.js
@@ -63,15 +63,20 @@ startSlideShow();
 
 const gnbToggleBtn = document.querySelector(".gnb_toggle_btn");
 const M_gnbToggleBtn = document.querySelector(".gnbMobile .gnb_toggle_btn");
+const lnb_close_btn = document.querySelector(".lnb_close_btn");
 
 gnbToggleBtn.addEventListener("click", () => {
   const lnbMain = document.querySelector(".lnbMain");
-  console.log("clicked");
-  lnbMain.classList.toggle("active");
+  const lnb_close_btn = document.querySelector(".lnb_close_btn");
+  lnbMain.classList.add("active");
+})
+
+lnb_close_btn.addEventListener("click", () => {
+  const lnbMain = document.querySelector(".lnbMain");
+  lnbMain.classList.remove("active");
 })
 
 M_gnbToggleBtn.addEventListener("click", () => {
-  
   const M_Header = document.querySelector(".gnb_container");
   const lnbMain = document.querySelector(".lnbMain");
   M_Header.style.background = "#222"
@@ -82,3 +87,103 @@ M_gnbToggleBtn.addEventListener("click", () => {
     M_Header.style.background = "#111";
   }
 })
+
+document.addEventListener("DOMContentLoaded" , function(){
+  const counter3 = document.querySelector(".counter-3");
+  for(let i = 0; i < 2; i++){
+    for(let j = 0; j < 10; j++){
+      const div = document.createElement("div");
+      div.className = "num";
+      div.textContent = j;
+      counter3.appendChild(div);
+    }
+  }
+
+  const finalDiv = document.createElement("div");
+  finalDiv.className = "num";
+  finalDiv.textContent = 0;
+  counter3.appendChild(finalDiv);
+  
+  function animate(counter, duration, delay = 0) {
+    const numHeight = counter.querySelector(".num").clientHeight;
+    const totalDistance = (counter.querySelectorAll(".num").length - 1) * numHeight;
+    gsap.to(counter, {
+      y: -totalDistance,
+      duration: duration,
+      delay: delay,
+      ease: "power2.inOut",
+    })
+  }
+
+  animate(counter3, 5);
+  animate(document.querySelector(".counter-2"), 6);
+  animate(document.querySelector(".counter-1"), 2, 4);
+  
+});
+
+gsap.to(".digit", {
+  top : "-200px",
+  stagger: {
+    amount: 0.25,
+  },
+  delay: 6,
+  duration : 1,
+  ease: "power4.inOut"
+});
+
+gsap.from(".loader-1", {
+  width : 0,
+  duration : 6,
+  ease : "power2.inOut",
+});
+
+gsap.from(".loader-2", {
+  width : 0,
+  delay : 0.5,
+  duration : 6,
+  ease : "power2.inOut",
+});
+
+gsap.to(".loader" , {
+  background : "none",
+  delay : 6,
+  duration: 0.1,
+})
+
+gsap.to(".loader-1", {
+  rotate : 90,
+  y : -50,
+  duration : 0.5,
+  delay : 6,
+})
+
+gsap.to(".loader-2", {
+  x: -75,
+  y: -175,
+  duration: 0.5,
+},
+"<"
+);
+
+gsap.to(".loader" , {
+  scale : 200,
+  duration : 1,
+  delay : 7,
+  ease : "power2.inOut"
+});
+
+gsap.to(".loader" , {
+  rotate : 45,
+  y : 500,
+  x : 2000,
+  duration : 1,
+  delay : 7,
+  ease : "power2.inOut",
+});
+
+gsap.to("#loading_screen", {
+  opacity : 0,
+  duration : 0.5,
+  delay : 7.5,
+  ease : "power1.inOut",
+});
